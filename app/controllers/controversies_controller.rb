7bb6e83179ea03cb4a83b572dd1dc2992810d8fa
Arnaud Levy <contact@arnaudlevy.com>
2023-11-16 17:25:44 +0100

    remove formats

--- app/controllers/controversies_controller.rb
+++ app/controllers/controversies_controller.rb
@@ -21,35 +21,25 @@ class ControversiesController < ApplicationController
 
   def create
     @controversy = Controversy.new(controversy_params)
-    respond_to do |format|
-      if @controversy.save
-        format.html { redirect_to controversy_url(@controversy), notice: "Controversy was successfully created." }
-        format.json { render :show, status: :created, location: @controversy }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @controversy.errors, status: :unprocessable_entity }
-      end
+    if @controversy.save
+      redirect_to controversy_url(@controversy), notice: "Controversy was successfully created."
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
   def update
-    respond_to do |format|
-      if @controversy.update(controversy_params)
-        format.html { redirect_to controversy_url(@controversy), notice: "Controversy was successfully updated." }
-        format.json { render :show, status: :ok, location: @controversy }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @controversy.errors, status: :unprocessable_entity }
-      end
+    # TODO manage empty editorjs data, crash
+    if @controversy.update(controversy_params)
+      redirect_to controversy_url(@controversy), notice: "Controversy was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
   def destroy
     @controversy.destroy
-    respond_to do |format|
-      format.html { redirect_to controversies_url, notice: "Controversy was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    redirect_to controversies_url, notice: "Controversy was successfully destroyed."
   end
 
   protected
