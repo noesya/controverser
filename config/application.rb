f6b2accf288ac13f7231ec14dc03e8873342bb29
Sébastien Gaya <sebastien.gaya@gmail.com>
2024-10-18 16:02:56 +0200

    app:update to 7.2.1.1

--- config/application.rb
+++ config/application.rb
@@ -21,16 +21,14 @@ Bundler.require(*Rails.groups)
 module Controverser
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
+    config.load_defaults 7.2
+
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w[assets tasks])
+
     config.i18n.available_locales = [:fr]
     config.i18n.default_locale = :fr
-
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
   end
 end
