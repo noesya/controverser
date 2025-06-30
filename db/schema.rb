76bf693f39595ca9b8800b78b4933b8c001a5e55
Sébastien Gaya <sebastien.gaya@gmail.com>
2025-06-30 15:38:51 +0200

    Ruby 3.4.4 + Rails 8

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_18_140017) do
+ActiveRecord::Schema[8.0].define(version: 2024_10_18_140017) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension "pg_catalog.plpgsql"
 
   create_table "concepts", force: :cascade do |t|
     t.string "title"
@@ -36,7 +36,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_18_140017) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "subtitle"
-    t.jsonb "data", default: {"blocks"=>[]}
+    t.jsonb "data", default: {"blocks" => []}
     t.text "data_rendered"
   end
 
